b035603b5ee0d5bca8ad41faa65a9526ffeb4385
carljame <carl@jovian34.com>
2020-08-08 16:44:12 -0400

    output file of duplicates for westlaw

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ combined_case_list = []
 for lexis_case in lexis_case_file.case_list:
     for westlaw_case in westlaw_case_file.case_list:
         if lexis_case[0] != westlaw_case[0]:
-            continue
+            pass
         else:
             words = []
             try:
@@ -56,11 +56,65 @@ for lexis_case in lexis_case_file.case_list:
                     combined_case_list.append(lexis_case)
                     break
 
+unique_westlaw_case_list = []
+for westlaw_case in westlaw_case_file.case_list:
+    westlaw_case_is_unique = True
+    for combined_case in combined_case_list:
+        if combined_case[0] != westlaw_case[0]:
+            pass
+        else:
+            words = []
+            try:
+                names = combined_case[1]
+            except IndexError:
+                continue
+            for name in names:
+                subnames = name.split()
+                for subname in subnames:
+                    words.append(subname)
+            for word in words:
+                if word in westlaw_case[1]:
+                    westlaw_case_is_unique = False
+                    break
+    if westlaw_case_is_unique:
+        unique_westlaw_case_list.append(westlaw_case)
+
+with open('outputs/unique_westlaw_cases.txt', 'w', encoding='utf-8') as result_file:
+    for case in unique_westlaw_case_list:
+        case_date = maya.MayaDT.from_datetime(case[0])
+        case_date = case_date.date
+        result_file.writelines(f"{case[1]}: on {case_date} result: {case[2]}, with phrases: {case[3]}\n")
+    unique_westlaw_case_count = len(unique_westlaw_case_list)
+    print(f"There are {unique_westlaw_case_count} unique Westlaw cases")
+    result_file.writelines("==============================================\n")
+    result_file.writelines(f"There were {unique_westlaw_case_count} unique Westlaw cases.\n")
+
+unique_lexis_case_list = []
+for lexis_case in lexis_case_file.case_list:
+    if lexis_case in combined_case_list:
+        continue
+    else:
+        unique_lexis_case_list.append(lexis_case)
+
 with open('outputs/combined_case.txt', 'w', encoding='utf-8') as result_file:
     for case in combined_case_list:
         case_date = maya.MayaDT.from_datetime(case[0])
         case_date = case_date.date
         result_file.writelines(f"{case[1]}: on {case_date} \n")
-    result_file.writelines("==============================================\n")
-    result_file.writelines(f"There were {len(combined_case_list)} combined cases "
+    combined_case_count = len(combined_case_list)
+    print(f"There were {combined_case_count} combined cases "
                            f"between Westlaw and Lexis")
+    result_file.writelines("==============================================\n")
+    result_file.writelines(f"There were {combined_case_count} combined cases "
+                           f"between Westlaw and Lexis")
+
+with open('outputs/unique_lexis_cases.txt', 'w', encoding='utf-8') as result_file:
+    for case in unique_lexis_case_list:
+        case_date = maya.MayaDT.from_datetime(case[0])
+        case_date = case_date.date
+        result_file.writelines(f"{case[1]}: on {case_date} \n")
+    unique_lexis_case_count = len(unique_lexis_case_list)
+    print(f"There are {unique_lexis_case_count} unique Lexis cases")
+    result_file.writelines("==============================================\n")
+    result_file.writelines(f"There were {unique_lexis_case_count} unique Lexis cases.\n")
+
